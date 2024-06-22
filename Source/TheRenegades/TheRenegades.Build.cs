5def423109f534f740df188426d5fcd54bc8bf40
samthebest000 <112246171+samthebest000@users.noreply.github.com>
2024-06-22 13:38:31 +0300

    Add project files.

---
+++ Source/TheRenegades/TheRenegades.Build.cs
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TheRenegades : ModuleRules
+{
+	public TheRenegades(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"GameplayTags",
+			"UMG",
+			"NetworkPrediction"
+		});
+	}
+}
---
+++ Source/TheRenegades/TheRenegadesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheRenegadesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATheRenegadesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATheRenegadesGameMode();
+};
+
+
+
