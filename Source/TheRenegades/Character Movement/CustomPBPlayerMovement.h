b8a665443a3456314eca03d6eb781a8ae3d43e4b
samthebest000 <samuel.esho@protonmail.com>
2024-07-05 17:04:31 +0300

    Abandoned Network Plugin, boilerplated new movement component and character to follow tutorial

---
+++ Source/TheRenegades/Character Movement/CustomPBPlayerMovement.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/PBPlayerMovement.h"
+#include "CustomPBPlayerMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THERENEGADES_API UCustomPBPlayerMovement : public UPBPlayerMovement
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TheRenegades/Character/PBBaseCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/PBPlayerCharacter.h"
+#include "PBBaseCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THERENEGADES_API APBBaseCharacter : public APBPlayerCharacter
+{
+	GENERATED_BODY()
+	
+};
